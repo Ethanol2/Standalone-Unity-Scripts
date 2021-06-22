b11a391562efc7f0c4945b1d308aa016f185d902
Ethan <technomaster1997@hotmail.com>
2021-06-22 14:30:43 -0400

    Update UXEffects.cs

--- UXEffects.cs
+++ UXEffects.cs
@@ -12,11 +12,14 @@ public class UXEffects : MonoBehaviour
         public GameObject element;      // Ref to Gameobject being transformed
         public Vector3 target;          // Target vector
         public float timeScale;         // Speed of transformation
+        public float tolerance;         // Distance at which the transformation is considered complete
     }
 
     List<UI_Transform> moveList = new List<UI_Transform>();     // Queue for elements translating
     List<UI_Transform> scaleList = new List<UI_Transform>();    // Queue for elements scaling
     List<UI_Transform> rotList = new List<UI_Transform>();      // Queue for elements rotating
+    public float defaultTime = 5f;
+    public float defaultTolerance = 0.01f;
     public int listCount;                                       // Total count for all lists
     public float lastDeltaTime;                                 // If the timescale is set to 0, use the last delta time
 
@@ -31,14 +34,16 @@ public class UXEffects : MonoBehaviour
     {
         // Check timescale
         lastDeltaTime = Time.timeScale > 0 ? Time.deltaTime : lastDeltaTime;
-        
+
         // Execute scaling
         for (int k = 0; k < scaleList.Count; k++)
         {
             scaleList[k].element.transform.localScale =
                 Vector3.Lerp(scaleList[k].element.transform.localScale, scaleList[k].target, scaleList[k].timeScale * lastDeltaTime);
 
-            if (Mathf.RoundToInt(scaleList[k].element.transform.localScale.magnitude * 1000f) == Mathf.RoundToInt(scaleList[k].target.magnitude * 1000f))
+            if (Mathf.Abs(scaleList[k].element.transform.localScale.x - scaleList[k].target.x) < moveList[k].tolerance &&
+                Mathf.Abs(scaleList[k].element.transform.localScale.y - scaleList[k].target.y) < moveList[k].tolerance &&
+                Mathf.Abs(scaleList[k].element.transform.localScale.z - scaleList[k].target.z) < moveList[k].tolerance)
             {
                 scaleList[k].element.transform.localScale = scaleList[k].target;
                 scaleList.RemoveAt(k);
@@ -52,7 +57,9 @@ public class UXEffects : MonoBehaviour
             moveList[k].element.transform.localPosition =
                 Vector3.Lerp(moveList[k].element.transform.localPosition, moveList[k].target, moveList[k].timeScale * lastDeltaTime);
 
-            if (moveList[k].element.transform.localPosition == moveList[k].target)
+            if (Mathf.Abs(moveList[k].element.transform.localPosition.x - moveList[k].target.x) < moveList[k].tolerance &&
+                Mathf.Abs(moveList[k].element.transform.localPosition.y - moveList[k].target.y) < moveList[k].tolerance &&
+                Mathf.Abs(moveList[k].element.transform.localPosition.z - moveList[k].target.z) < moveList[k].tolerance)
             {
                 moveList[k].element.transform.localPosition = moveList[k].target;
                 moveList.RemoveAt(k);
@@ -66,7 +73,9 @@ public class UXEffects : MonoBehaviour
             rotList[k].element.transform.localRotation =
                 Quaternion.Lerp(rotList[k].element.transform.localRotation, Quaternion.Euler(rotList[k].target), rotList[k].timeScale * lastDeltaTime);
 
-            if (rotList[k].element.transform.localRotation == Quaternion.Euler(rotList[k].target))
+            if (Mathf.Abs(rotList[k].element.transform.localEulerAngles.x - rotList[k].target.x) < rotList[k].tolerance &&
+                Mathf.Abs(rotList[k].element.transform.localEulerAngles.y - rotList[k].target.y) < rotList[k].tolerance &&
+                Mathf.Abs(rotList[k].element.transform.localEulerAngles.z - rotList[k].target.z) < rotList[k].tolerance)
             {
                 rotList[k].element.transform.localRotation = Quaternion.Euler(rotList[k].target);
                 rotList.RemoveAt(k);
@@ -74,7 +83,7 @@ public class UXEffects : MonoBehaviour
             }
         }
 
-        listCount = moveList.Count;
+        listCount = moveList.Count + scaleList.Count + rotList.Count;
 
     }
 
@@ -87,7 +96,8 @@ public class UXEffects : MonoBehaviour
                 UI_Transform replace;
                 replace.element = a_element;
                 replace.target = a_scale;
-                replace.timeScale = 5f;
+                replace.timeScale = defaultTime;
+                replace.tolerance = defaultTolerance;
                 scaleList[k] = replace;
                 return;
             }
@@ -96,7 +106,8 @@ public class UXEffects : MonoBehaviour
         UI_Transform temp;
         temp.element = a_element;
         temp.target = a_scale;
-        temp.timeScale = 5f;
+        temp.timeScale = defaultTime;
+        temp.tolerance = defaultTolerance;
         scaleList.Add(temp);
         return;
     }
@@ -112,7 +123,8 @@ public class UXEffects : MonoBehaviour
                     UI_Transform replace;
                     replace.element = a_element;
                     replace.target = a_position;
-                    replace.timeScale = 5f;
+                    replace.timeScale = defaultTime;
+                    replace.tolerance = defaultTolerance;
                     moveList[k] = replace;
                     return;
                 }
@@ -122,7 +134,8 @@ public class UXEffects : MonoBehaviour
         UI_Transform temp;
         temp.element = a_element;
         temp.target = a_position;
-        temp.timeScale = 5f;
+        temp.timeScale = defaultTime;
+        temp.tolerance = defaultTolerance;
         moveList.Add(temp);
         return;
     }
@@ -136,7 +149,8 @@ public class UXEffects : MonoBehaviour
                 UI_Transform replace;
                 replace.element = a_element;
                 replace.target = a_rot;
-                replace.timeScale = 5f;
+                replace.timeScale = defaultTime;
+                replace.tolerance = defaultTolerance;
                 rotList[k] = replace;
                 return;
             }
@@ -145,12 +159,13 @@ public class UXEffects : MonoBehaviour
         UI_Transform temp;
         temp.element = a_element;
         temp.target = a_rot;
-        temp.timeScale = 5f;
+        temp.timeScale = defaultTime;
+        temp.tolerance = defaultTolerance;
         rotList.Add(temp);
         return;
     }
 
-    public void scaleElement(GameObject a_element, Vector3 a_scale, float a_time)
+    public void scaleElement(GameObject a_element, Vector3 a_scale, float a_time, float a_tolerance)
     {
         for (int k = 0; k < scaleList.Count; k++)
         {
@@ -160,6 +175,7 @@ public class UXEffects : MonoBehaviour
                 replace.element = a_element;
                 replace.target = a_scale;
                 replace.timeScale = a_time;
+                replace.tolerance = a_tolerance;
                 scaleList[k] = replace;
                 return;
             }
@@ -169,11 +185,12 @@ public class UXEffects : MonoBehaviour
         temp.element = a_element;
         temp.target = a_scale;
         temp.timeScale = a_time;
+        temp.tolerance = a_tolerance;
         scaleList.Add(temp);
         return;
     }
 
-    public void moveElement(GameObject a_element, Vector3 a_position, float a_time)
+    public void moveElement(GameObject a_element, Vector3 a_position, float a_time, float a_tolerance)
     {
         if (moveList.Count > 0)
         {
@@ -185,6 +202,7 @@ public class UXEffects : MonoBehaviour
                     replace.element = a_element;
                     replace.target = a_position;
                     replace.timeScale = a_time;
+                    replace.tolerance = a_tolerance;
                     moveList[k] = replace;
                     return;
                 }
@@ -195,11 +213,12 @@ public class UXEffects : MonoBehaviour
         temp.element = a_element;
         temp.target = a_position;
         temp.timeScale = a_time;
+        temp.tolerance = a_tolerance;
         moveList.Add(temp);
         return;
     }
 
-    public void rotateElement(GameObject a_element, Vector3 a_rot, float a_time)
+    public void rotateElement(GameObject a_element, Vector3 a_rot, float a_time, float a_tolerance)
     {
         for (int k = 0; k < rotList.Count; k++)
         {
@@ -209,6 +228,7 @@ public class UXEffects : MonoBehaviour
                 replace.element = a_element;
                 replace.target = a_rot;
                 replace.timeScale = a_time;
+                replace.tolerance = a_tolerance;
                 rotList[k] = replace;
                 return;
             }
@@ -218,6 +238,7 @@ public class UXEffects : MonoBehaviour
         temp.element = a_element;
         temp.target = a_rot;
         temp.timeScale = a_time;
+        temp.tolerance = a_tolerance;
         rotList.Add(temp);
         return;
     }
